973e1be203ac60b084062d8398a27fb486374b4b
fulvaz <fulvaz@foxmail.com>
2017-07-26 15:02:43 +0800

    finish helper functions

--- index.js
+++ index.js
@@ -33,10 +33,18 @@ function coLike(gen) {
   }
 }
 
+// 其实是一个比较重要的函数, co的扩展都是在这里进行的
+// 对类型不同的输入都转换到一个特定的协议进行处理---这里是Promise
 function toPromise(obj) {
   // 检查输入变量
   if (!obj) return obj
-  if ()
+  if (isPromise) return obj
+  if (isGenerator) coLike(obj)
+  // 将function转换为promise
+  // 这里假设你的函数是一个thunk---同一标准, 不然没法处理了
+  // 另外, 都会很正常地认为你传进来的是一个异步函数. 当然不是异步的也没关系, 同步代码会在promise中全执行了
+  if ('function' === typeof obj)  
+  // co这里还扩展了对array于object输入的处理, 我就不管了
 }
 
 function isPromise(obj) {
@@ -57,3 +65,13 @@ function isGenerator(obj) {
   return 'function' === typeof conProto.next && 'function' === typeof conProto.throw
 }
 
+function thunkToPromise(thunkFunc) {
+  return new Promise((resolve, reject) => {
+    thunkFunc((err, res) => {
+      // 当有多个返回值的话, 就以数组的形式resolve多个
+      if (err) reject(err)
+      if (arguments.length > 2) res = arguments.slice(1)
+      resolve(res)
+    })
+  })
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
+~~ A simple repository for research on async in javascript~~
+
 A simplified tj/co for research
